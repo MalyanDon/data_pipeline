802cab87ad847eb4bbe04eb8686ba1f464e1157a
Abhishek Malyan <abhishekmalyan@Abhisheks-MacBook-Air-6.local>
2025-07-01 15:36:21 +0530

    🔧 Fix MongoDB SSL connection for Render deployment

--- Database/config.js
+++ Database/config.js
@@ -3,7 +3,7 @@ const config = {
     username: 'abhishekmalyan2',
     password: 'STLKamQJJoUWv0Ks',
     cluster: 'database.tu83c8a.mongodb.net',
-    uri: process.env.MONGODB_URI || 'mongodb+srv://abhishekmalyan2:STLKamQJJoUWv0Ks@database.tu83c8a.mongodb.net/',
+    uri: process.env.MONGODB_URI || 'mongodb+srv://abhishekmalyan2:STLKamQJJoUWv0Ks@database.tu83c8a.mongodb.net/?retryWrites=true&w=majority&appName=Database&ssl=true&tlsAllowInvalidCertificates=true',
     database: process.env.MONGODB_DATABASE || 'financial_data_2025'
   },
   postgresql: process.env.NODE_ENV === 'production' 
